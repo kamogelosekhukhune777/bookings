6f085fe699f849dd34b03d9201f98cd82f2af187
Kamogelo Sekhukhune <kamogelosekhukhune7@gmail.com>
2024-08-13 00:38:27 +0200

    client front-end added

---
+++ api/cmd/services/bookings/main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"expvar"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+	"time"
+
+	"github.com/ardanlabs/conf/v3"
+	"go.uber.org/zap"
+
+	"github.com/kamogelosekhukhune777/bookings/api/http/api/debug"
+	"github.com/kamogelosekhukhune777/bookings/foundation/logger"
+)
+
+var build = "development"
+
+func main() {
+
+	// Construct the application logger.
+	log, err := logger.New("bookings")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer log.Sync()
+
+	//=====================================================================================================
+
+	ctx := context.Background()
+
+	if err := run(ctx, log); err != nil {
+		log.Error(ctx, "startup", "err", err)
+		os.Exit(1)
+	}
+
+}
+
+func run(ctx context.Context, log *zap.SugaredLogger) error {
+
+	//=====================================================================================================
+	// GOMAXPROCS
+
+	log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0), "build", build)
+
+	//=====================================================================================================
+	// Configuration
+
+	cfg := struct {
+		conf.Version
+		Web struct {
+			ReadTimeout        time.Duration `conf:"default:5s"`
+			WriteTimeout       time.Duration `conf:"default:10s"`
+			IdleTimeout        time.Duration `conf:"default:120s"`
+			ShutdownTimeout    time.Duration `conf:"default:20s"`
+			APIHost            string        `conf:"default:0.0.0.0:3000"`
+			DebugHost          string        `conf:"default:0.0.0.0:3010"`
+			CORSAllowedOrigins []string      `conf:"default:*,mask"`
+		}
+	}{
+		Version: conf.Version{
+			Build: build,
+			Desc:  "pure-cart",
+		},
+	}
+
+	const prefix = "pure-cart"
+	help, err := conf.Parse(prefix, &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			return nil
+		}
+		return fmt.Errorf("parsing config: %w", err)
+	}
+
+	//=====================================================================================================
+	// App Starting
+
+	log.Info(ctx, "starting service", "version", cfg.Build)
+	defer log.Info(ctx, "shutdown complete")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return fmt.Errorf("generating config for output: %w", err)
+	}
+	log.Info(ctx, "startup", "config", out)
+
+	expvar.NewString("build").Set(cfg.Build)
+
+	//=====================================================================================================
+	// Start Debug Service
+
+	go func() {
+		log.Info(ctx, "startup", "status", "debug v1 router started", "host", cfg.Web.DebugHost)
+
+		if err := http.ListenAndServe(cfg.Web.DebugHost, debug.Mux()); err != nil {
+			log.Error(ctx, "shutdown", "status", "debug v1 router closed", "host", cfg.Web.DebugHost, "msg", err)
+		}
+	}()
+
+	//=====================================================================================================
+	// Start API Service
+
+	log.Info(ctx, "startup", "status", "initializing V1 API support")
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+
+	api := http.Server{
+		Addr:         cfg.Web.APIHost,
+		Handler:      nil,
+		ReadTimeout:  cfg.Web.ReadTimeout,
+		WriteTimeout: cfg.Web.WriteTimeout,
+		IdleTimeout:  cfg.Web.IdleTimeout,
+		ErrorLog:     logger.NewStdLogger(log, logger.LevelError),
+	}
+
+	serverErrors := make(chan error, 1)
+
+	go func() {
+		log.Info(ctx, "startup", "status", "api router started", "host", api.Addr)
+
+		serverErrors <- api.ListenAndServe()
+	}()
+
+	//=====================================================================================================
+	// Shutdown
+
+	select {
+	case err := <-serverErrors:
+		return fmt.Errorf("server error: %w", err)
+
+	case sig := <-shutdown:
+		log.Info(ctx, "shutdown", "status", "shutdown started", "signal", sig)
+		defer log.Info(ctx, "shutdown", "status", "shutdown complete", "signal", sig)
+
+		ctx, cancel := context.WithTimeout(ctx, cfg.Web.ShutdownTimeout)
+		defer cancel()
+
+		if err := api.Shutdown(ctx); err != nil {
+			api.Close()
+			return fmt.Errorf("could not stop server gracefully: %w", err)
+		}
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -3,10 +3,13 @@ module github.com/kamogelosekhukhune777/bookings
 go 1.22.1
 
 require (
-	github.com/ardanlabs/conf v1.5.0
+	github.com/ardanlabs/conf/v3 v3.1.7
+	github.com/arl/statsviz v0.6.0
 	github.com/dimfeld/httptreemux v5.0.1+incompatible
-	go.uber.org/automaxprocs v1.5.3
 	go.uber.org/zap v1.27.0
 )
 
-require go.uber.org/multierr v1.10.0 // indirect
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+)
